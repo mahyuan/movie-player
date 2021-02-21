c6d1f71c94250c389ab09f3e22751aa4cddad83c
mahyuan <dsz9258@163.com>
2021-02-21 22:28:27 +0800

    Update: env

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const request = axios.create({
   // baseURL: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_BASE_API : '',
   baseURL: import.meta.env.VITE_ENV === 'prod' ? import.meta.env.VITE_BASE_API : '',
-  withCredentials: true,
+  // withCredentials: true,
   timeout: 30000
 })
 
